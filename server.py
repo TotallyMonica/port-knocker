9fe835bff6f6a20cdec4a89932a92d342ac5fca7
Monica Hanson <wolfmurdurer23@gmail.com>
2023-02-17 16:34:30 -0600

    Began implementing a UDP check

--- server.py
+++ server.py
@@ -7,8 +7,42 @@ import sys
 import os
 import csv
 
+def test_udp(port, interface='0.0.0.0', timeout=60, verbose=False):
+    data = f'Test to see if UDP traffic is working'
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as srv:
+        srv.settimeout(timeout)
+
+        try:
+            srv.bind((interface, port))
+        except OSError:
+            return "In use"
+    
+    srv.listen()
+
+    if verbose:
+        print(f"Waiting for a connection on {interface}:{port}...")
+    
+    try:
+        conn, addr = srv.accept()
+    except TimeoutError:
+        return False
+
+    if verbose:
+        print(f"Connection from {addr}")
+
+    # If there's a connection, confirm connectivity by sending the received data back.
+    conn.send(data.encode('utf-8'))
+    test = conn.recv(2048)
+    conn.send(test)
+    test = conn.recv(2048).decode('utf-8')
+    if test == data:
+        return True
+    
+    return "Malformed"
+
 # Testing method
-def test(port, interface='0.0.0.0', timeout=60, verbose=False):
+def test_tcp(port, interface='0.0.0.0', timeout=60, verbose=False):
     # Create the test string to be used to confirm connectivity
     data = f'The cat is out of the bag'
 
@@ -45,7 +79,7 @@ def test(port, interface='0.0.0.0', timeout=60, verbose=False):
         return "Malformed"
     
 # Loop tests all the ports, handles the logging
-def loop(ports, interface='0.0.0.0', timeout=10, verbose=False, knownGood=None):
+def loop(ports, interface='0.0.0.0', proto='tcp', timeout=10, verbose=False, knownGood=None):
     results = []
     checkedPorts = []
 
@@ -57,30 +91,45 @@ def loop(ports, interface='0.0.0.0', timeout=10, verbose=False, knownGood=None):
     else:
         checkedPorts = ports
 
-    for port in checkedPorts:
-        result = test(port, interface, timeout, verbose)
+    if proto == 'tcp':
+        for port in checkedPorts:
+            result = test_tcp(port, interface, timeout, verbose)
 
-        # Warn if the port failed and the  
-        if not result and verbose:
-            print(f'Warning: Port {port} failed!')
-        
-        if result == True:
-            results.append([port, "Pass"])
-        elif result == False:
-            results.append([port, "Fail"])
-        else:
-            results.append([port, result])
-        
+            # Warn if the port failed and the  
+            if not result and verbose:
+                print(f'Warning: Port {port} failed!')
+            
+            if result == True:
+                results.append([port, "Pass"])
+            elif result == False:
+                results.append([port, "Fail"])
+            else:
+                results.append([port, result])
+    
+    elif proto == 'udp':
+        for port in checkedPorts:
+            result = test_udp(port, interface, timeout, verbose)
+
+            # Warn if the port failed and the  
+            if not result and verbose:
+                print(f'Warning: Port {port} failed!')
+            
+            if result == True:
+                results.append([port, "Pass"])
+            elif result == False:
+                results.append([port, "Fail"])
+            else:
+                results.append([port, result])
     
     return results
 
 # The stupid way I have to get the return value because I can't just do it natively
-def newThread(output, threadNum, startPort, endPort, address, timeout, verbose, knownGood):
-    output[threadNum] = loop(range(startPort, endPort), address, timeout, verbose, knownGood)
+def newThread(output, threadNum, startPort, endPort, address, protocol, timeout, verbose, knownGood):
+    output[threadNum] = loop(range(startPort, endPort), address, protocol, timeout, verbose, knownGood)
 
 # Begin threading, restricted to 8 threads as of right now
 # TODO: Clean this spaghetti code up
-def beginThreading(startPort, endPort, address, timeout, verbose, knownGood):
+def beginThreading(startPort, endPort, address, protocol, timeout, verbose, knownGood):
     output = [None, None, None, None, None, None, None, None]
     offset = int( (endPort - startPort) / 8 )
     nextStartingPort = startPort
@@ -96,14 +145,14 @@ def beginThreading(startPort, endPort, address, timeout, verbose, knownGood):
     
     print(portRanges)
 
-    thread1 = threading.Thread(target=newThread, args=(output, 0, portRanges[0][0], portRanges[0][1], address, timeout, verbose, knownGood))
-    thread2 = threading.Thread(target=newThread, args=(output, 1, portRanges[1][0], portRanges[1][1], address, timeout, verbose, knownGood))
-    thread3 = threading.Thread(target=newThread, args=(output, 2, portRanges[2][0], portRanges[2][1], address, timeout, verbose, knownGood))
-    thread4 = threading.Thread(target=newThread, args=(output, 3, portRanges[3][0], portRanges[3][1], address, timeout, verbose, knownGood))
-    thread5 = threading.Thread(target=newThread, args=(output, 4, portRanges[4][0], portRanges[4][1], address, timeout, verbose, knownGood))
-    thread6 = threading.Thread(target=newThread, args=(output, 5, portRanges[5][0], portRanges[5][1], address, timeout, verbose, knownGood))
-    thread7 = threading.Thread(target=newThread, args=(output, 6, portRanges[6][0], portRanges[6][1], address, timeout, verbose, knownGood))
-    thread8 = threading.Thread(target=newThread, args=(output, 7, portRanges[7][0], portRanges[7][1], address, timeout, verbose, knownGood))
+    thread1 = threading.Thread(target=newThread, args=(output, 0, portRanges[0][0], portRanges[0][1], address, protocol, timeout, verbose, knownGood))
+    thread2 = threading.Thread(target=newThread, args=(output, 1, portRanges[1][0], portRanges[1][1], address, protocol, timeout, verbose, knownGood))
+    thread3 = threading.Thread(target=newThread, args=(output, 2, portRanges[2][0], portRanges[2][1], address, protocol, timeout, verbose, knownGood))
+    thread4 = threading.Thread(target=newThread, args=(output, 3, portRanges[3][0], portRanges[3][1], address, protocol, timeout, verbose, knownGood))
+    thread5 = threading.Thread(target=newThread, args=(output, 4, portRanges[4][0], portRanges[4][1], address, protocol, timeout, verbose, knownGood))
+    thread6 = threading.Thread(target=newThread, args=(output, 5, portRanges[5][0], portRanges[5][1], address, protocol, timeout, verbose, knownGood))
+    thread7 = threading.Thread(target=newThread, args=(output, 6, portRanges[6][0], portRanges[6][1], address, protocol, timeout, verbose, knownGood))
+    thread8 = threading.Thread(target=newThread, args=(output, 7, portRanges[7][0], portRanges[7][1], address, protocol, timeout, verbose, knownGood))
     
     thread1.start()
     thread2.start()
@@ -127,19 +176,44 @@ def beginThreading(startPort, endPort, address, timeout, verbose, knownGood):
 
 # Configure the server to check ports
 def main():
+    PROTO_DEFAULT = 'tcp'
     interface = '0.0.0.0'
     timeout = 10
     knownGood = None
     verbose = True
     startPort = 1
     endPort = 65535
+    proto = PROTO_DEFAULT
+
+    if '-p' in sys.argv or '--protocol' in sys.argv:
+        try:
+            index = sys.argv.index('-p')
+        except ValueError:
+            index = sys.argv.index('--protocol')
+        
+        try:
+            proto = sys.argv[index + 1]
+        except IndexError:
+            print(F"No protocol provided, defaulting to {PROTO_DEFAULT}")
+        
+        if proto.lower() == 'tcp' or proto.lower() == 'udp':
+            pass
+        else:
+            print(f"Invalid or unsupported protocol {proto}, defaulting to TCP")
 
     if '-i' in sys.argv or '--interface' in sys.argv:
-        index = sys.argv.index('-i')
+        try:
+            index = sys.argv.index('-i')
+        except ValueError:
+            index = sys.argv.index('--interface')
         interface = sys.argv[index + 1]
 
     if '-t' in sys.argv or '--timeout' in sys.argv:
-        index = sys.argv.index('-t')
+        try:
+            index = sys.argv.index('-t')
+        except ValueError:
+            index = sys.argv.index('--timeout')
+
         if sys.argv[index + 1].isdigit():
             timeout = int(sys.argv[index + 1])
         else:
@@ -147,7 +221,10 @@ def main():
 
     if '-g' in sys.argv or '--known-good' in sys.argv:
         knownGood = []
-        index = sys.argv.index('-g')
+        try:
+            index = sys.argv.index('-g')
+        except ValueError:
+            index = sys.argv.index('--known-good')
         val = sys.argv[index + 1].split(',')
 
         # Validate it is a valid port
@@ -171,10 +248,10 @@ def main():
     if os.getuid() != 0:
         startPort = 1024
     
-    results = beginThreading(startPort, endPort + 1, interface, timeout, verbose, knownGood)
+    results = beginThreading(startPort, endPort + 1, interface, proto, timeout, verbose, knownGood)
     print(results)
 
-    with open('results.csv', 'w') as filp:
+    with open(f'results-{proto}.csv', 'w') as filp:
         writer = csv.writer(filp, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(['port', 'result'])
         for row in results:
